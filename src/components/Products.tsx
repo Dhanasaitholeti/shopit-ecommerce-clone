97c3e9bd3731445601ee045567bd03f3a9405a52
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-11-02 21:25:41 +0530

    added filtering functinality based on category

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,15 +1,29 @@
 import ProductCard from "./ProductCard";
 import { ProductsWrapper } from "./ui/Product.styles";
 import { products } from "../config/data";
+import { useLocation } from "react-router-dom";
 
 const Products = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+
+  const category = queryParams.getAll("category");
+  console.log(category);
+
+  let newlist;
+
+  if (category.length < 1) newlist = products;
+  else {
+    newlist = products.filter((entry) => entry.Category == category[0]);
+  }
+
   return (
     <>
       <ProductsWrapper>
         <h1>All Products</h1>
         <div>
-          {products.map((entry) => (
-            <ProductCard product={entry} />
+          {newlist.map((entry) => (
+            <ProductCard product={entry} key={entry.Product_ID} />
           ))}
         </div>
       </ProductsWrapper>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,18 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { SectionsWrapper, SidebarWrapper } from "./ui/Sidebar.styles";
 
 const SideBar = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <SidebarWrapper>
         <h3>Category</h3>
         <SectionsWrapper>
-          <p>Clothing</p>
-          <p>Electronics</p>
-          <p>Appliances</p>
-          <p>Grocery</p>
-          <p>Toys</p>
+          <p onClick={() => navigate("?category=Clothing")}>Clothing</p>
+          <p onClick={() => navigate("?category=Electronics")}>Electronics</p>
+          <p onClick={() => navigate("?category=Appliances")}>Appliances</p>
+          <p onClick={() => navigate("?category=Grocery")}>Grocery</p>
+          <p onClick={() => navigate("?category=Toys")}>Toys</p>
         </SectionsWrapper>
-        <h3>Ratings</h3>
       </SidebarWrapper>
     </>
   );

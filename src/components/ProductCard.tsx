d466c27613f5f75184a96265f7a11b520cc429b3
Dhanasai Tholeti <dhanasai5566@gmail.com>
2023-11-02 14:59:08 +0530

    added demo data

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+const ProductCard = () => {
+  return (
+    <>
+      <div>
+        <h1>This is going to render</h1>
+      </div>
+    </>
+  );
+};
+
+export default ProductCard;
---
+++ src/config/data.ts
@@ -0,0 +1,500 @@
+export const products = [
+  {
+    Category: "Clothing",
+    Product_ID: "CLO123",
+    Price: "$29.99",
+    Rating: 4.5,
+    Description:
+      "A comfortable and stylish t-shirt made from high-quality cotton.",
+    Image_URL: "https://example.com/clo123.jpg",
+    Title: "Men's Cotton T-Shirt",
+  },
+
+  {
+    Category: "Clothing",
+    Product_ID: "CLO456",
+    Price: "$49.99",
+    Rating: 4.2,
+    Description:
+      "A trendy and fashionable dress for women, perfect for any occasion.",
+    Image_URL: "https://example.com/clo456.jpg",
+    Title: "Women's Floral Print Dress",
+  },
+  {
+    Category: "Clothing",
+    Product_ID: "CLO789",
+    Price: "$39.99",
+    Rating: 4.0,
+    Description: "Classic denim jeans for a casual and timeless look.",
+    Image_URL: "https://example.com/clo789.jpg",
+    Title: "Men's Denim Jeans",
+  },
+  {
+    Category: "Clothing",
+    Product_ID: "CLO012",
+    Price: "$59.99",
+    Rating: 4.7,
+    Description: "A cozy and warm sweater made from soft wool.",
+    Image_URL: "https://example.com/clo012.jpg",
+    Title: "Women's Wool Sweater",
+  },
+  {
+    Category: "Clothing",
+    Product_ID: "CLO345",
+    Price: "$19.99",
+    Rating: 4.3,
+    Description:
+      "Lightweight and breathable sports shorts for active individuals.",
+    Image_URL: "https://example.com/clo345.jpg",
+    Title: "Men's Sports Shorts",
+  },
+  {
+    Category: "Clothing",
+    Product_ID: "CLO678",
+    Price: "$69.99",
+    Rating: 4.8,
+    Description: "A stylish and elegant evening gown for special occasions.",
+    Image_URL: "https://example.com/clo678.jpg",
+    Title: "Women's Evening Gown",
+  },
+  {
+    Category: "Clothing",
+    Product_ID: "CLO901",
+    Price: "$34.99",
+    Rating: 4.1,
+    Description:
+      "Comfortable and stretchy yoga pants for yoga and fitness enthusiasts.",
+    Image_URL: "https://example.com/clo901.jpg",
+    Title: "Women's Yoga Pants",
+  },
+  {
+    Category: "Clothing",
+    Product_ID: "CLO234",
+    Price: "$24.99",
+    Rating: 4.5,
+    Description: "A versatile and trendy denim jacket for a fashionable look.",
+    Image_URL: "https://example.com/clo234.jpg",
+    Title: "Men's Denim Jacket",
+  },
+  {
+    Category: "Clothing",
+    Product_ID: "CLO567",
+    Price: "$39.99",
+    Rating: 4.6,
+    Description: "Stylish and durable running shoes for athletes and runners.",
+    Image_URL: "https://example.com/clo567.jpg",
+    Title: "Men's Running Shoes",
+  },
+  {
+    Category: "Clothing",
+    Product_ID: "CLO890",
+    Price: "$29.99",
+    Rating: 4.4,
+    Description: "A casual and comfortable hoodie for everyday wear.",
+    Image_URL: "https://example.com/clo890.jpg",
+    Title: "Women's Hoodie",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE123",
+    Price: "$199.99",
+    Rating: 4.8,
+    Description:
+      "A powerful and sleek smartphone with a high-resolution display and advanced camera features.",
+    Image_URL: "https://example.com/ele123.jpg",
+    Title: "Smartphone",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE456",
+    Price: "$99.99",
+    Rating: 4.4,
+    Description:
+      "An affordable and compact laptop with a fast processor and long battery life.",
+    Image_URL: "https://example.com/ele456.jpg",
+    Title: "Laptop",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE789",
+    Price: "$149.99",
+    Rating: 4.6,
+    Description:
+      "A feature-packed smartwatch with fitness tracking and notification capabilities.",
+    Image_URL: "https://example.com/ele789.jpg",
+    Title: "Smartwatch",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE012",
+    Price: "$499.99",
+    Rating: 4.7,
+    Description:
+      "A high-definition television with a large screen and immersive sound.",
+    Image_URL: "https://example.com/ele012.jpg",
+    Title: "Smart TV",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE345",
+    Price: "$79.99",
+    Rating: 4.2,
+    Description:
+      "A compact and portable Bluetooth speaker for music on the go.",
+    Image_URL: "https://example.com/ele345.jpg",
+    Title: "Bluetooth Speaker",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE678",
+    Price: "$349.99",
+    Rating: 4.5,
+    Description:
+      "A versatile and powerful DSLR camera for photography enthusiasts.",
+    Image_URL: "https://example.com/ele678.jpg",
+    Title: "DSLR Camera",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE901",
+    Price: "$29.99",
+    Rating: 4.0,
+    Description:
+      "True wireless earbuds with noise cancellation and long battery life.",
+    Image_URL: "https://example.com/ele901.jpg",
+    Title: "True Wireless Earbuds",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE234",
+    Price: "$199.99",
+    Rating: 4.3,
+    Description:
+      "A gaming console with high-performance graphics and immersive gameplay.",
+    Image_URL: "https://example.com/ele234.jpg",
+    Title: "Gaming Console",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE567",
+    Price: "$79.99",
+    Rating: 4.1,
+    Description:
+      "A compact and lightweight e-reader for reading your favorite books on the go.",
+    Image_URL: "https://example.com/ele567.jpg",
+    Title: "E-Reader",
+  },
+  {
+    Category: "Electronics",
+    Product_ID: "ELE890",
+    Price: "$249.99",
+    Rating: 4.6,
+    Description:
+      "A wireless router with fast internet speeds and wide coverage range.",
+    Image_URL: "https://example.com/ele890.jpg",
+    Title: "Wireless Router",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP123",
+    Price: "$299.99",
+    Rating: 4.6,
+    Description:
+      "A versatile and energy-efficient refrigerator with ample storage space.",
+    Image_URL: "https://example.com/app123.jpg",
+    Title: "Refrigerator",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP456",
+    Price: "$149.99",
+    Rating: 4.3,
+    Description:
+      "A powerful vacuum cleaner with multiple attachments for efficient cleaning.",
+    Image_URL: "https://example.com/app456.jpg",
+    Title: "Vacuum Cleaner",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP789",
+    Price: "$79.99",
+    Rating: 4.5,
+    Description:
+      "An easy-to-use coffee maker that brews delicious coffee in minutes.",
+    Image_URL: "https://example.com/app789.jpg",
+    Title: "Coffee Maker",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP012",
+    Price: "$199.99",
+    Rating: 4.4,
+    Description:
+      "A high-capacity washing machine with advanced washing cycles for different fabric types.",
+    Image_URL: "https://example.com/app012.jpg",
+    Title: "Washing Machine",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP345",
+    Price: "$99.99",
+    Rating: 4.2,
+    Description:
+      "A compact microwave oven with multiple cooking presets for convenient cooking.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Microwave Oven",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP678",
+    Price: "$349.99",
+    Rating: 4.7,
+    Description:
+      "A durable and efficient dishwasher for hassle-free dishwashing.",
+    Image_URL: "https://example.com/app678.jpg",
+    Title: "Dishwasher",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP901",
+    Price: "$59.99",
+    Rating: 4.1,
+    Description:
+      "A powerful blender for making smoothies, shakes, and other delicious recipes.",
+    Image_URL: "https://example.com/app901.jpg",
+    Title: "Blender",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP234",
+    Price: "$129.99",
+    Rating: 4.5,
+    Description:
+      "A versatile air fryer for healthy and crispy cooking with minimal oil.",
+    Image_URL: "https://example.com/app234.jpg",
+    Title: "Air Fryer",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP567",
+    Price: "$199.99",
+    Rating: 4.3,
+    Description:
+      "An energy-efficient air conditioner for keeping your home cool during hot summers.",
+    Image_URL: "https://example.com/app567.jpg",
+    Title: "Air Conditioner",
+  },
+  {
+    Category: "Appliances",
+    Product_ID: "APP890",
+    Price: "$79.99",
+    Rating: 4.6,
+    Description: "A compact and lightweight iron for wrinkle-free clothes.",
+    Image_URL: "https://example.com/app890.jpg",
+    Title: "Iron",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO123",
+    Price: "$2.99",
+    Rating: 4.7,
+    Description: "Fresh and organic fruits sourced directly from local farms.",
+    Image_URL: "https://example.com/gro123.jpg",
+    Title: "Organic Fruits Basket",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO456",
+    Price: "$1.99",
+    Rating: 4.2,
+    Description:
+      "A pack of aromatic and high-quality coffee beans for coffee enthusiasts.",
+    Image_URL: "https://example.com/gro456.jpg",
+    Title: "Coffee Beans",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO789",
+    Price: "$3.49",
+    Rating: 4.4,
+    Description: "A variety pack of nutritious and delicious granola bars.",
+    Image_URL: "https://example.com/gro789.jpg",
+    Title: "Granola Bars",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO123",
+    Price: "$2.99 per pound",
+    Rating: 4.5,
+    Description:
+      "Organic Apples - These organic apples are grown without the use of synthetic pesticides or fertilizers. They are crisp, juicy, and packed with natural sweetness. Enjoy them as a healthy snack or use them in your favorite recipes.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Organic Apples",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO456",
+    Price: "$3.49",
+    Rating: 4.2,
+    Description:
+      "Whole Grain Bread - This whole grain bread is made from high-quality, nutrient-rich grains. It is a delicious and healthy option for sandwiches, toasts, or as a side with soups and salads.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Whole Grain Bread",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO789",
+    Price: "$1.99 per container",
+    Rating: 4.0,
+    Description:
+      "Greek Yogurt - This creamy and tangy Greek yogurt is a versatile and nutritious option. It can be enjoyed on its own, used as a topping, or incorporated into smoothies and recipes.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Greek Yogurt",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO012",
+    Price: "$9.99",
+    Rating: 4.7,
+    Description:
+      "Extra Virgin Olive Oil - This high-quality extra virgin olive oil is perfect for drizzling over salads, pasta, or bread. It adds a rich and flavorful touch to your dishes and is a staple in Mediterranean cuisine.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Extra Virgin Olive Oil",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO345",
+    Price: "$4.99 per pound",
+    Rating: 4.3,
+    Description:
+      "Organic Quinoa - This organic quinoa is a nutritious and versatile grain. It can be used as a base for salads, added to stir-fries, or used as a substitute for rice in various recipes.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Organic Quinoa",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO678",
+    Price: "$6.99",
+    Rating: 4.8,
+    Description:
+      "Almond Butter - This smooth and creamy almond butter is made from roasted almonds. It is a delicious and nutritious spread that can be enjoyed on toast, used in baking, or added to smoothies.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Almond Butter",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO901",
+    Price: "$2.99 per bundle",
+    Rating: 4.1,
+    Description:
+      "Organic Spinach - This organic spinach is packed with vitamins and minerals. It is a versatile leafy green that can be used in salads, sautéed as a side dish, or added to smoothies for a nutrient boost.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Organic Spinach",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO234",
+    Price: "$3.99 per dozen",
+    Rating: 4.6,
+    Description:
+      "Free-Range Eggs - These free-range eggs come from chickens that are raised in a more natural environment. They are known for their rich flavor and vibrant yolks, making them a popular choice for cooking and baking.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Free-Range Eggs",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO567",
+    Price: "$3.99 per pound",
+    Rating: 4.4,
+    Description:
+      "Organic Quinoa - This organic quinoa is a versatile and nutritious grain. It is gluten-free and can be used as a base for salads, side dishes, or as a substitute for rice or pasta.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "Organic Quinoa",
+  },
+  {
+    Category: "Grocery",
+    Product_ID: "GRO890",
+    Price: "$2.99 per bar",
+    Rating: 4.3,
+    Description:
+      "Dark Chocolate - Indulge your sweet tooth with this rich and decadent dark chocolate. It is made with high-quality cocoa beans and has a smooth and intense flavor. Enjoy it as a treat or use it in baking recipes.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: " Dark Chocolate",
+  },
+  {
+    Category: "Toys",
+    Product_ID: "TOY123",
+    Price: "$19.99",
+    Rating: 4.5,
+    Description:
+      "LEGO Classic Creative Bricks Set - This LEGO set includes a variety of colorful bricks and pieces, allowing children to unleash their creativity and build anything they can imagine. It is a great starter set for young builders.",
+    Image_URL: "https://example.com/app345.jpg",
+    Title: "LEGO Classic Creative Bricks Set",
+  },
+  {
+    Category: "Toys",
+    Product_ID: "TOY456",
+    Price: "$149.99",
+    Rating: 4.2,
+    Description:
+      "Barbie Dreamhouse - The Barbie Dreamhouse is a modern and stylish dollhouse that offers endless play possibilities. It features multiple rooms, interactive accessories, and even a working elevator.",
+    Image_URL: "https://example.com/app345.jpg",
+
+    Title: "Barbie Dreamhouse",
+  },
+  {
+    Category: "Toys",
+    Product_ID: "TOY789",
+    Price: "$12.99",
+    Rating: 4.0,
+    Description:
+      "Nerf N-Strike Elite Disruptor Blaster - The Nerf N-Strike Elite Disruptor Blaster is a fun and action-packed toy. It allows kids to engage in exciting Nerf battles with its rapid-fire feature and includes six foam darts.",
+    Image_URL: "https://example.com/app345.jpg",
+
+    Title: "Nerf N-Strike Elite Disruptor Blaster",
+  },
+  {
+    Category: "Toys",
+    Product_ID: "TOY012",
+    Price: "$34.99",
+    Rating: 4.7,
+    Description:
+      "LEGO Friends Heartlake City Amusement Pier - This LEGO Friends set lets children build an exciting amusement pier with various rides and attractions. It offers hours of creative play and storytelling.",
+    Image_URL: "https://example.com/app345.jpg",
+
+    Title: "LEGO Friends Heartlake City Amusement Pier",
+  },
+  {
+    Category: "Toys",
+    Product_ID: "TOY345",
+    Price: "$29.99",
+    Rating: 4.3,
+    Description:
+      "Hot Wheels 20 Car Gift Pack - This Hot Wheels gift pack includes 20 die-cast cars in various styles and designs. It's a fantastic collection for young car enthusiasts and collectors.",
+    Image_URL: "https://example.com/app345.jpg",
+
+    Title: "Hot Wheels 20 Car Gift Pack",
+  },
+  {
+    Category: "Toys",
+    Product_ID: "TOY678",
+    Price: "$39.99",
+    Rating: 4.8,
+    Description:
+      "Melissa & Doug Wooden Building Blocks Set - This wooden block set provides endless building possibilities for kids. It promotes creativity, problem-solving, and fine motor skills.",
+    Image_URL: "https://example.com/app345.jpg",
+
+    Title: "Melissa & Doug Wooden Building Blocks Set",
+  },
+  {
+    Category: "Toys",
+    Product_ID: "TOY901",
+    Price: "$24.99",
+    Rating: 4.1,
+    Description:
+      "Paw Patrol Ultimate Rescue Fire Truck - Join the Paw Patrol pups on their ultimate rescue missions with this fire truck toy. It features a working ladder, water cannon, and sounds.",
+    Image_URL: "https://example.com/app345.jpg",
+
+    Title: "Paw Patrol Ultimate Rescue Fire Truck",
+  },
+];
